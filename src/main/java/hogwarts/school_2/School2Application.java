7706a53e61c46547870740077e4a7e4a50288109
alex <alexsemen666653@gmail.com>
2024-04-19 14:19:24 +0300

    Initial commit

---
+++ src/main/java/hogwarts/school_2/School2Application.java
@@ -0,0 +1,14 @@
+package hogwarts.school_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School2Application.class, args);
+		System.out.println("Hello, world");
+	}
+
+}
